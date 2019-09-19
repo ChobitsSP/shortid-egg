cb03b44130c6cf7a947636546c091b36728dac24
Chobits <chobits@live.cn>
2019-09-19 16:23:24 +0800

    1

--- app/router.ts
+++ app/router.ts
@@ -4,6 +4,6 @@ export default (app: Application) => {
   const { router, controller } = app;
 
   router.get("/", controller.home.index);
-  router.get("/api/Shortid/SetUrl", controller.home.SetUrl);
+  router.get("/api/url/shorten", controller.home.SetUrl);
   router.get("/r/:id", controller.home.info);
 };
